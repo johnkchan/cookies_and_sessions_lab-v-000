f4a11100702c2bf07bc3436cf72e75b54d228c2d
Jonathan Chan <johnkchan252@gmail.com>
2019-05-22 04:11:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :cart
   
-  def add_to_cart
-    # Get the item from the path
-    @item = Item.find(params[:id])
-   
-    # Load the cart from the session, or create a new empty cart.
-    current_cart << @item.id
-  end
   
   def cart
     session[:cart] ||= []
